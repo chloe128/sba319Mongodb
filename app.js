c2e4fd97f51c79229b4e6d9ffb392885301bb9ff
chloe128 <chloemeng98@gmail.com>
2024-07-23 21:19:40 -0700

    2nd  commit

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 require("dotenv").config();
 const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3000;
+const methodOverride = require("method-override");
 const conn = require("./db/conn");
+conn();
 const userRoutes = require("./routes/userRoutes");
 const bookRoutes = require("./routes/bookRoutes");
 const reviewRoutes = require("./routes/reviewRoutes");
+const Book = require("./models/Book");
+const Review = require("./models/Review");
+const User = require("./models/User");
+
+//view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 //middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 app.use("/users", userRoutes);
 app.use("/books", bookRoutes);
 app.use("/reviews", reviewRoutes);
 
-const app = express();
-conn();
+//route
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,24 +2,29 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
+// Get all users
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.render("users_index", { users });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Get form to create new user
+router.get("/new", (req, res) => {
+  res.render("users_new.ejs");
+});
+
 // Create a new user
 router.post("/", async (req, res) => {
   try {
     const user = new User(req.body);
     await user.save();
-    res.status(201).send(user);
+    res.redirect("/users");
   } catch (error) {
     res.status(400).send(error);
-  }
-});
-
-// Get all users
-router.get("/", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.send(users);
-  } catch (error) {
-    res.status(500).send(error);
   }
 });
 
@@ -30,7 +35,20 @@ router.get("/:id", async (req, res) => {
     if (!user) {
       return res.status(404).send();
     }
-    res.send(user);
+    res.render("users_show.ejs", { user });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Get form to edit a user by ID
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.render("users_edit.ejs", { user });
   } catch (error) {
     res.status(500).send(error);
   }
@@ -46,7 +64,7 @@ router.patch("/:id", async (req, res) => {
     if (!user) {
       return res.status(404).send();
     }
-    res.send(user);
+    res.redirect(`/users/${user._id}`);
   } catch (error) {
     res.status(400).send(error);
   }
@@ -59,7 +77,7 @@ router.delete("/:id", async (req, res) => {
     if (!user) {
       return res.status(404).send();
     }
-    res.send(user);
+    res.redirect("/users");
   } catch (error) {
     res.status(500).send(error);
   }
